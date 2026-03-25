baeed4596ae65ae8deab6ec099b321b9134a8a7d
Batyr <bshekish@gmail.com>
2026-03-25 13:23:21 +0500

    chore: add typeorm migration setup and initial migration

---
+++ tandem-trauma-api/data-source.ts
@@ -0,0 +1,16 @@
+import 'tsconfig-paths/register';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export default new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
+});
